f03b563c7e890c048f9bce7b4f5b31b0853f871d
Pxiaobao <ababyseven@163.com>
2022-04-21 23:31:56 +0800

    基本uniapp

---
+++ pages/classify/classify.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<view v-for="(item,index) in stu" :key="index">
+			序号：{{index}}, 姓名：{{item.name}}, 分数：{{item.age}}
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				stu:[
+						{name:'张三',age:20},
+						{name:'李四',age:30},
+						{name:'王五',age:10},
+				]
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/home/home.vue
@@ -0,0 +1,67 @@
+<template>
+	<view>
+			<view class="fs-30">
+				欢迎使用uniapp开发
+			</view>
+			<view class="fs-24"> 
+				欢迎使用uniapp开发
+			</view>
+			<view class="fs-26">
+				欢迎使用uniapp开发
+			</view>
+			<view class="title">
+					<text class="iconfont icon-vip-fill"></text>
+			</view>
+			<text>{{msg}}</text>
+			<input v-model="msg"></input>
+			<!-- 动态使用属性(v-bind可省略)的三种方式 -->
+			<view class="box" v-bind:class="isActive?'active':''">
+			</view>
+			<view v-bind:class="['box',isActive?'active':'']">
+			</view>
+			<view v-bind:class="['box',{'active':isActive}]">
+			</view>
+			<!-- 动态使用src属性-->
+			<image v-bind:src=imags[loginState] mode=""></image>
+			<button v-on:tap="changeimg" type="primary">控制图片变化</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				msg:'这是数据',
+				isActive:false,
+				loginState:0,
+				imags:[
+					'../../static/img/banner.jpg',
+					'../../static/img/french.jpg'
+				]
+				
+			};
+		},
+		methods:{
+			changeimg:function(){
+				this.loginState = this.loginState===0?1:0
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.title{
+	color: #007AFF;
+}
+.icon-vip-fill{
+	font-size: 100rpx;
+}
+.box{
+	height: 200rpx;
+	width: 200rpx;
+	border: 1px solid red;
+}
+.box.active{
+	background-color: #007AFF;
+}
+</style>
